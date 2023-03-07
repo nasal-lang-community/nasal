8209ab380ef454beb431580f024225ef79170cdf
Erik Hofman <erik@ehofman.com>
2023-03-07 09:17:33 +0100

    Fix for: Saikrishna Arcot: In simgear, the file simgear/nasal/nasal-bin.c was added a couple months ago. That file has an #include for config.h, but there is no config.h generated for simgear as far as I can tell.

--- nasal-bin.c
+++ nasal-bin.c
@@ -1,17 +1,16 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
+//#include <sys/types.h>
 #include <sys/stat.h>
 
 #ifdef _WIN32
 #include <windows.h>
 #else
-#include <unistd.h>
-#include <pthread.h>
+//#include <unistd.h>
+//#include <pthread.h>
 #endif
 
-#include "config.h"
 #include "nasal.h"
 #include "data.h"
 
